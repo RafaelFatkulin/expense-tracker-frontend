d8a019a182422ba176ef23951ae70dbae78adc6a
RafaelFatkulin <armistman@vk.com>
2024-02-16 14:09:51 +0500

    [add] tag updating and color-picker current color outline

--- src/features/tag/update/update-tag.ui.tsx
+++ src/features/tag/update/update-tag.ui.tsx
@@ -1,8 +1,12 @@
 import type { HTMLAttributes } from 'react';
 import { useState, forwardRef } from 'react';
-import { Pencil } from 'lucide-react';
-import type { Tag } from '~entities/tag';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Pencil, Save } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import type { Tag, UpdateTagDto } from '~entities/tag';
+import { useUpdateTagMutation, UpdateTagDtoSchema } from '~entities/tag';
 import { Button } from '~shared/ui/button';
+import { ColorPicker } from '~shared/ui/color-picker';
 import {
   Dialog,
   DialogContent,
@@ -11,6 +15,9 @@ import {
   DialogTitle,
   DialogTrigger
 } from '~shared/ui/dialog';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '~shared/ui/form';
+import { Input } from '~shared/ui/input';
+import { Loader } from '~shared/ui/loader';
 
 type Props = {
   tag: Tag;
@@ -19,6 +26,30 @@ type Props = {
 export const UpdateTagButton = forwardRef<HTMLButtonElement, Props>(({ tag }: Props, ref) => {
   const [open, setOpen] = useState<boolean>(false);
   const toggle = () => setOpen(!open);
+
+  const form = useForm({
+    resolver: zodResolver(UpdateTagDtoSchema),
+    defaultValues: {
+      title: tag.title || '',
+      color: tag.color || ''
+    }
+  });
+
+  const { mutate, isPending } = useUpdateTagMutation(tag.id);
+
+  const onSubmit = (values: UpdateTagDto) =>
+    mutate(
+      {
+        id: tag.id,
+        updateTagDto: values
+      },
+      {
+        onSuccess: () => {
+          toggle();
+          form.reset();
+        }
+      }
+    );
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -38,7 +69,49 @@ export const UpdateTagButton = forwardRef<HTMLButtonElement, Props>(({ tag }: Pr
           <DialogTitle>Редактировать тэг</DialogTitle>
           <DialogDescription>Введите новую информацию для тэга.</DialogDescription>
         </DialogHeader>
-        {tag.title}
+
+        <Form {...form}>
+          <form className='grid gap-3' onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              name='title'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Название</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Tag #1' {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='color'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Цвет</FormLabel>
+                  <FormControl>
+                    <div>
+                      <Input placeholder='Tag #1' type='hidden' {...field} />
+                      <ColorPicker value={form.getValues('color')} setValue={form.setValue} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type='submit' className='w-full sm:w-auto sm:ml-0 mr-auto' disabled={isPending}>
+              {isPending ? (
+                <Loader variant='sm' className='mr-2' />
+              ) : (
+                <Save className='size-4 mr-2' />
+              )}
+              Сохранить
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
--- src/shared/ui/color-picker.tsx
+++ src/shared/ui/color-picker.tsx
@@ -2,6 +2,7 @@ import type { KeyboardEvent } from 'react';
 import { memo } from 'react';
 import { Palette } from 'lucide-react';
 import type { UseFormSetValue } from 'react-hook-form';
+import { cn } from '~shared/lib/cn';
 import { colors } from '~shared/lib/colors';
 import { Button } from '~shared/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '~shared/ui/popover';
@@ -40,16 +41,19 @@ export const ColorPicker = memo(({ value, setValue }: Props) => {
           )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent align='start' className='w-64'>
+      <PopoverContent align='start' className='w-[17rem]'>
         <ScrollArea className='w-full h-[200px] rounded scroll-pr-2.5'>
-          <div className=' flex flex-row flex-wrap gap-1'>
+          <div className='flex flex-row flex-wrap gap-1.5'>
             {colors.map((color) => (
               <i
                 tabIndex={0}
                 role='button'
                 key={color}
                 aria-label={color}
-                className='w-5 h-5 rounded cursor-pointer'
+                className={cn(
+                  'w-5 h-5 rounded cursor-pointer',
+                  color === value && 'outline outline-offset-1 outline-primary'
+                )}
                 style={{ backgroundColor: color }}
                 onClick={() => onClick(color)}
                 onKeyDown={(e) => onKeyDown(e, color)}
